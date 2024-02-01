af549dc5167180992994458fe63988daf7b27d31
arafat-sabbir <arafatshabbir8@gmail.com>
2024-02-01 15:51:20 +0600

    Added Detail Page

--- src/Pages/MovieDetail/MovieDetail.jsx
+++ src/Pages/MovieDetail/MovieDetail.jsx
@@ -1,16 +1,20 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
 
 const MovieDetail = () => {
     const location = useLocation()
-    console.log(location.state);
+    const movieDetail = location.state;
     return (
         <div>
+            <Link to={'/'}> <p className="go-home-btn">{<FaHome />}Go Home</p></Link>
             <div className="detail-container">
                 <div className="left-side-detail">
-
+                    <img className="detail-image" src={movieDetail.show?.image?.original} alt="" />
                 </div>
                 <div className="right-side-detail">
-
+                    <h1>{movieDetail.show.name}</h1>
+                    <p>{movieDetail?.show?.premiered?.slice(0, 4)}</p>
+                    <p className="movie-summary">{movieDetail?.show?.summary?.slice(3, movieDetail.show.summary.length - 4)}</p>
                 </div>
             </div>
         </div>
