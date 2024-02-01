782c5f68524845ff7e87817a254eb39847939288
arafat-sabbir <arafatshabbir8@gmail.com>
2024-02-01 16:27:44 +0600

    Added Book Ticket Form

--- src/Pages/MovieDetail/MovieDetail.jsx
+++ src/Pages/MovieDetail/MovieDetail.jsx
@@ -1,7 +1,10 @@
 import { Link, useLocation } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
+import { useState } from "react";
 
 const MovieDetail = () => {
+    const [formOpen, setFormOpen] = useState(false)
+    console.log(formOpen);
     const location = useLocation()
     const movieDetail = location.state;
     return (
@@ -15,7 +18,21 @@ const MovieDetail = () => {
                     <h1>{movieDetail.show.name}</h1>
                     <p>{movieDetail?.show?.premiered?.slice(0, 4)}</p>
                     <p className="movie-summary">{movieDetail?.show?.summary?.slice(3, movieDetail.show.summary.length - 4)}</p>
+                    <div>
+                        <button onClick={() => setFormOpen(!formOpen)} className="booking-btn">Book A Ticket</button>
+                        <a target="blank" href={movieDetail?.show?.url}><button className="watch-btn">Watch Now</button></a>
+                    </div>
                 </div>
+                {
+                    formOpen ? <form className={`${formOpen ? "booking-form-active" : "booking-form"}`} action="">
+                        <input readOnly={true} defaultValue={movieDetail.show?.name} className="booking-input" type="text" name="" id="" />
+                        <input className="booking-input" placeholder="Name" type="text" name="name" id="" />
+                        <input className="booking-input" type="email" placeholder="Your Email" name="email" id="" />
+                        <input className="booking-input" type="number" placeholder="Number Of Person" name="numberOfPerson" id="" />
+                        <input placeholder="Your Phone" className="booking-input" type="text" name="phone" id="" />
+                        <input className="booking-btn" type="submit" value={"Book Ticket"} />
+                    </form> : ""
+                }
             </div>
         </div>
     );
